8cc5084accf53f4a611be69959a0014dcdc326e8
Shivammani19 <shivamtripathi214@gmail.com>
2021-01-18 23:42:07 +0530

    rn-starter

---
+++ rn-starter/src/screens/ComponentsScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Text,StyleSheet} from 'react-native'
+
+const ComponentsScreen=()=>{
+    return <Text style={styles.textStyle}>Hash_define</Text>
+
+};
+
+const styles=StyleSheet.create({
+    textStyle:{fontSize:30}
+});
+export default ComponentsScreen;
